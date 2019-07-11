ec0addfd998bb4be01dbfbd553d1e7fe471b7599
Kevin Tran <mai_le09@yahoo.com>
2019-07-11 11:15:14 -0700

    Created an init function to call on another function with that needs to almost be immediately invoke within the module

--- app.js
+++ app.js
@@ -19,24 +19,33 @@ var budgetController = (function(){
 
 //Handles the UI
 var UIController = (function(){
+    /*This object helps make it so that you have to repeat yourself and if you ever decide to change the name of the data type you wont have to manually change it for each line of code */
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
     
     
-    
-    
-    /*Returns the function 'getInput' to the controller, therefore making it public*/
+    /*Returns the function 'getInput' and 'getDOMstrings' to the controller, therefore making it public*/
     return {
         getInput: function(){
     //Returns the current stored values the user has inputted by returning them in an object
             return {
-            //Will be either inc or exp
-            type: document.querySelector('.add__type').value,
+            //Value will be either inc or exp
+            type: document.querySelector(DOMstrings.inputType).value,
         
-            description: document.querySelector('.add__description').value,
+            description: document.querySelector(DOMstrings.inputDescription).value,
             
-            value: document.querySelector('.add__value').value
-            }          
-    }
-    }
+            value: document.querySelector(DOMstrings.inputValue).value
+            };          
+    },
+    /* Return the HTML class values, AKA DOM strings, so that the code targetted could also be reused by the controll module */
+      getDOMstrings: function(){
+          return DOMstrings;
+      }  
+    };
 })();
 
 
@@ -47,23 +56,14 @@ var UIController = (function(){
 //Parameters that are 'predicted to be passed' and receive and arguments
 var controller = (function(budgetCtrl, UICtrl){
     
-    var ctrlAddItem = function(){
-        // 1. Get the filed input data
-            var input = UICtrl.getInput();
-            console.log(input);
-        // 2. Add the item to the budget controller
+    //Helps organize the module 
+    var setupEventListeners = function(){
         
-        // 3. Add the item to the UI
+    /*Stores the DOM strings inside the variable so the controllter could also reuse the targetted strings*/
+    var DOM = UICtrl.getDOMstrings();    
         
-        // 4. Calculate the budget
-        
-        // 5. Display the budget on the UI
-        
-        console.log('It works');
-    }
-    
     /*If the user presses the button then the function ctrlAddItem activates. If you were to put () after the ctrlAddItem it would immediately invoke the function rather than wait to be called on*/
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     
     /*if any key is 'pressed' then activate the function and pass that key into the event parameter*/
     document.addEventListener('keypress', function(event){
@@ -73,12 +73,39 @@ var controller = (function(budgetCtrl, UICtrl){
        ctrlAddItem();  
      }
         
-    });
+    });        
+    };
+    
+    
+    //Function that adds the item to the screen
+    var ctrlAddItem = function(){
+        // 1. Get the filed input data
+            var input = UICtrl.getInput();
+        // 2. Add the item to the budget controller
+        
+        // 3. Add the item to the UI
+        
+        // 4. Calculate the budget
+        
+        // 5. Display the budget on the UI
+        
+        console.log('It works');
+    };
+    
+    //Makes it so that the 'setupEventListener' is public and returned
+    return {
+        init: function(){
+            console.log('Application has started');
+            //Calls up this function and invokes it
+            setupEventListeners();
+        }
+    };
+    
 //The arguments     
 })(budgetController, UIController);
 
-
-
+/*Initializes the the setupEventListener function that is in the IIFE 'controller'. The point is to immediately invoke it */
+controller.init();
 
 
 
