b3c99d835c4b68a54813903e3d74f64388b759c2
Kevin Tran <mai_le09@yahoo.com>
2019-07-16 11:14:18 -0700

    Added live date feature

--- app.js
+++ app.js
@@ -159,7 +159,8 @@ var UIController = (function(){
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel:'.item__percentage'
+        expensesPercLabel:'.item__percentage',
+        dateLabel:'.budget__title--month'
     };
     
         var formatNumber = function(num, type){
@@ -285,6 +286,18 @@ var UIController = (function(){
         });
         
     },    
+    
+    displayMonth: function(){
+        var now, year, month, months;
+        now = new Date();
+        
+        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        
+        month = now.getMonth();
+        
+        year = now.getFullYear();
+        document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },    
         
     /* Return the HTML class values, AKA DOM strings, so that the code targetted could also be reused by the controll module */
       getDOMstrings: function(){
@@ -389,6 +402,7 @@ var controller = (function(budgetCtrl, UICtrl){
     return {
         init: function(){
             console.log('Application has started');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
